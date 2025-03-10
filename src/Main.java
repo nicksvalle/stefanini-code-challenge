bf55f29095ffff14b1a7008590f78394cb557b42
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2025-03-10 18:01:19 -0300

    isDivisibleByThree finish

--- src/Main.java
+++ src/Main.java
@@ -27,4 +27,12 @@ public class Main {
         }
     }
 
+    public static String isDivisibleByThree(int num){
+        if (num % 3 == 0) {
+            return num + " is divisible by 3";
+        } else{
+           return num + " is not divisible by 3";
+        }
+    }
+
 }
